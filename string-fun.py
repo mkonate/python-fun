e3088f3ad58eb80a02eba60f9753a62942b0421d
Mohamed Konate <moslastar@gmail.com>
2017-06-10 01:26:15 -0400

    Playing with len() and formartting

--- string-fun.py
+++ string-fun.py
@@ -3,3 +3,5 @@ message_2 = "This is another basic string; this time, in double quotes"
 
 print(message)
 print(message_2)
+
+print('The lenght of the string "{}" is {}.'.format(message, len(message)))
